a9d5ca8779bbdbe51799deaa0d6397c5aba5fda7
RajFrom5Bix <rajnayal@5bix.in>
2025-08-18 16:56:03 +0530

    establish DB

---
+++ server/lib/db.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+export const connectDB = async () =>{
+
+    try {
+
+        mongoose.connection.on('connected', () => {
+            console.log("MongoDB connection established");
+        });
+
+        await mongoose.connect(`${process.env.MONGODB_URI}/chat-app`, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log("MongoDB connected successfully");
+    } catch (error) {
+        console.error("MongoDB connection failed:", error);
+        process.exit(1);
+    }
+
+}
--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import "dotenv/config";
 import cors from 'cors';
 import http from 'http';
+import { connectDB } from './lib/db.js';
 
 
 const app = express();
@@ -14,6 +15,10 @@ app.use(express.json({limit:'4mb'}));
 app.use("/api/status", (req,res) =>{
     res.send("server is live")
 })
+
+
+//connecting to the database
+await connectDB();
 
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, ()=>{
