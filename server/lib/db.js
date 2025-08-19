9f085a8cad85b99e99e704c52f8a9b20972dd1cd
RajFrom5Bix <rajnayal@5bix.in>
2025-08-19 21:27:19 +0530

    minor change

--- server/lib/db.js
+++ server/lib/db.js
@@ -5,7 +5,7 @@ export const connectDB = async () =>{
     try {
 
         mongoose.connection.on('connected', () => {
-            console.log("MongoDB connection established");
+            console.log("MongoDB connection established!!");
         });
 
         await mongoose.connect(`${process.env.MONGODB_URI}/chat-app`, {
